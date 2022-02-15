96c5472ca795fbf4e7d1743c10b833fe4e1ed5f6
dominikgorczyca <dgx32123@gmail.com>
2022-02-15 12:50:00 +0100

    array functions

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-function d($value, $function = '')
+
+use function PHPSTORM_META\type;
+
+function d(mixed $value, string $function = ''): void
 {
-    if($function === 'p') {
+    if ($function === 'p') {
         echo "<pre>";
         print_r($value);
         echo "</pre>";
@@ -10,12 +13,11 @@ function d($value, $function = '')
         var_dump($value);
         echo "</pre>";
     }
-
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-     
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,21 +25,34 @@ function d($value, $function = '')
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <?php
-        $array1 = ["gr" => 3, "nutshell" => 4];
-        $array2 = [2, 299, 10 => 33];
-        $units = ["Ah" => 'me', 'you', "noice" => 'them', 'wizard'];
-        echo array_push($units, ...$array2);
-        $units[array_keys($array1)[0]] = '3';
-        d($units, 'p');
-        echo array_pop($units);
-        d($units, 'p');
-        d(array_splice($units, 5, 1));
-        d($units, 'p');
-        d(array_shift($units));
-        d(array_shift($units));
-        d($units, 'p');
+
+    class KECZ
+    {
+        private $legs;
+        private $whiskers;
+
+        public function __construct($a, $b)
+        {
+            $this -> legs = $a;
+            $this -> whiskers = $b;
+            d("a: {$this -> legs} i b: {$this -> whiskers}");
+        }
+    }
+
+    $array1 = [new KECZ(10, 20), ["gr" => 3, "nutshell" => 4], 4, '4', 5, '5'];
+
+    d($array1, 'p');
+    d(in_array(new KECZ(10, 20), $array1, true));
+    d(spl_object_hash($array1[0]));
+    
+    $object = &$array1[0];
+    d(spl_object_hash($object));
+    d(in_array($object, $array1, true));
+    // d(in_array(spl_object_hash($object), $array1, true));
+
 
     ?>
 </body>
