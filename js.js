d64ad1f2672a407a9e48c3422d71c6b043acaa91
dominikgorczyca <dgx32123@gmail.com>
2022-02-20 19:38:02 +0100

    JS async part 2

--- js.js
+++ js.js
@@ -1,6 +1,23 @@
-console.log(1);
-setTimeout(() => {
-    console.log(2);
-}, 1000);
-console.log(3);
-console.log(4);
+fetch('https://jsonplaceholder.typicode.com/todos/10')
+  .then(response => response.json())
+  .then(json => {
+        // console.log(json);
+    });
+
+// console.log(fetch('https://jsonplaceholder.typicode.com/todos/10'));
+
+let request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+   if(request.readyState === 4) {
+       console.log(request.responseText);
+        // document.getElementById("title").innerText = request.responseText;
+        document.getElementById("completed").innerText = request.responseText;
+   }
+});
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.send()
+
+
+
